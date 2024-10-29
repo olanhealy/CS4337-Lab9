3890ab5930153790df1d239ecac84e5d09ee3c43
Olan Healy <olanhealy12@gmail.com>
2024-10-29 21:26:51 +0000

    Steps 1-3

---
+++ demo/src/main/java/com/example/notificationservice/NotificationMessage.java
@@ -0,0 +1,43 @@
+package com.example.notificationservice;
+
+public class NotificationMessage {
+    private String message;
+    private String channelType;
+    private String routingKey;
+
+
+    public NotificationMessage() {}
+
+
+    public NotificationMessage(String message, String channelType, String routingKey) {
+        this.message = message;
+        this.channelType = channelType;
+        this.routingKey = routingKey;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getChannelType() {
+        return channelType;
+    }
+
+    public void setChannelType(String channelType) {
+        this.channelType = channelType;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+
+}
